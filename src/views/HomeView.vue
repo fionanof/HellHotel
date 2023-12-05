27fe33d203aa94cd04cdddc4bba316c02a96f04b
Fiona Abraha <nofabraha@gmail.com>
2023-12-05 23:43:07 +0100

    picturelink_on _home_site_test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,31 @@
-<script setup></script>
+<script >
+import sourceData from '@/data.json'
+
+export default {
+  data() {
+    return {
+      destinations: sourceData.destinations
+    }
+  }
+}
+</script>
 
 <template>
-  <main>
-    <h1>HomeView</h1>
-  </main>
+  
+  <div class="home">
+    <h1>Home</h1>
+    <div class="destinations">
+      <router-link 
+          v-for="destination in destinations"
+          :key="destination.id"
+          :to ="destination.slug"
+          >
+        
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/src/images/${destination.image}`" :alt="destination.name">
+    
+     </router-link>
+    </div>
+  </div>
+
 </template>
